61ee78659ae0c4348ee9bb793ba54700b43b40e6
zhengyangyong <yangyong.zheng@huawei.com>
2018-04-09 18:51:49 +0800

    add dark launch implement
    Signed-off-by: zhengyangyong <yangyong.zheng@huawei.com>

--- edge-service/src/main/java/org/apache/servicecomb/scaffold/edge/EdgeDispatcher.java
+++ edge-service/src/main/java/org/apache/servicecomb/scaffold/edge/EdgeDispatcher.java
@@ -1,72 +1,82 @@
 package org.apache.servicecomb.scaffold.edge;
 
-import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
-
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.servicecomb.edge.core.AbstractEdgeDispatcher;
-import org.apache.servicecomb.edge.core.CompatiblePathVersionMapper;
 import org.apache.servicecomb.edge.core.EdgeInvocation;
-import org.apache.servicecomb.serviceregistry.definition.DefinitionConst;
-import org.apache.servicecomb.swagger.invocation.exception.InvocationException;
+import org.apache.servicecomb.scaffold.edge.darklaunch.DarkLaunchRule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.config.DynamicPropertyFactory;
+import com.netflix.config.DynamicStringProperty;
 
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.CookieHandler;
 
 /**
- * This is a default edge dispatcher match two style path url :
- * 1. /serviceName/version/operation
- * like /user-service/v1/login?name=xxx&password=xxx
- * 2. /serviceName/operation
- * like /user-service/login?name=xxx&password=xxx
- * this style will use latest version of active instance
+ *match /serviceName/operation
  */
 public class EdgeDispatcher extends AbstractEdgeDispatcher {
-  private final CompatiblePathVersionMapper versionMapper = new CompatiblePathVersionMapper();
+  private static final ObjectMapper OBJ_MAPPER = new ObjectMapper();
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(EdgeDispatcher.class);
+
+  private final Map<String, DynamicStringProperty> properties = new ConcurrentHashMap<>();
+
+  private final Map<String, DarkLaunchRule> rules = new HashMap<>();
 
   public int getOrder() {
     return 10000;
   }
 
   public void init(Router router) {
-    String regex = "/([^\\\\/]+)/([^\\\\/]+)/(.*)";
-    router.routeWithRegex(regex).handler(CookieHandler.create());
-    router.routeWithRegex(regex).handler(createBodyHandler());
-    router.routeWithRegex(regex).failureHandler(this::onFailure).handler(this::onRequest);
-
-    regex = "/([^\\\\/]+)/(.*)";
+    String regex = "/([^\\\\/]+)/(.*)";
     router.routeWithRegex(regex).handler(CookieHandler.create());
     router.routeWithRegex(regex).handler(createBodyHandler());
     router.routeWithRegex(regex).failureHandler(this::onFailure).handler(this::onRequest);
   }
 
-  protected void onRequest(RoutingContext context) {
+  private void onRequest(RoutingContext context) {
     Map<String, String> pathParams = context.pathParams();
-    String serviceName = pathParams.get("param0");
-    String versionRule;
-    String path;
-    if (pathParams.size() == 3) {
-      String pathVersion = pathParams.get("param1");
-      path = "/" + pathParams.get("param2");
-      versionRule = versionMapper.getOrCreate(pathVersion).getVersionRule();
-    } else if (pathParams.size() == 2) {
-      versionRule = DefinitionConst.VERSION_RULE_ALL;
-      path = "/" + pathParams.get("param1");
-    } else {
-      throw new InvocationException(BAD_REQUEST,
-          "request url must be /serviceName/version/operation or /serviceName/operation");
+    final String service = pathParams.get("param0");
+    String path = "/" + pathParams.get("param1");
+
+    String serviceName = DynamicPropertyFactory.getInstance()
+        .getStringProperty("edge.routes." + service + ".service", service).get();
+
+    if (!rules.containsKey(service)) {
+      properties.computeIfAbsent(service, s -> {
+        DynamicStringProperty property = DynamicPropertyFactory.getInstance()
+            .getStringProperty("edge.routes." + service + ".dark-launch-rules", "");
+        rules.put(service, parseRule(property.getValue()));
+        //灰度发布配置更新的时候更新缓存
+        property.addCallback(() -> rules.put(service, parseRule(property.getValue())));
+        return property;
+      });
     }
 
     EdgeInvocation edgeInvocation = new EdgeInvocation();
-    edgeInvocation.setVersionRule(versionRule);
-
-    serviceName = DynamicPropertyFactory.getInstance()
-        .getStringProperty("edge.routes." + serviceName, serviceName).get();
+    edgeInvocation.setVersionRule(rules.get(service).matchVersion(context.request().headers().entries()));
 
     edgeInvocation.init(serviceName, context, path, httpServerFilters);
     edgeInvocation.edgeInvoke();
+  }
+
+  private DarkLaunchRule parseRule(String config) {
+    try {
+      if (StringUtils.isNotEmpty(config)) {
+        return OBJ_MAPPER.readValue(config, DarkLaunchRule.class);
+      }
+    } catch (IOException e) {
+      LOGGER.error("parse rule failed", e);
+    }
+    return new DarkLaunchRule();
   }
 }
